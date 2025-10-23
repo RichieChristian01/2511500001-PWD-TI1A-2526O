78b722f625a306fa07b5c893102c1da7d5d59381
Richie Christian <2511500001@mahasiswa.atmaluhur.ac.id>
2025-10-23 15:28:52 +0700

    membuat styling untuk selector ID section#about styling input dan textarea

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,3 +1,4 @@
 document.getElementById("menuToggle").addEventListener("click", function () { 
     document.querySelector("nav").classList.toggle("active");
+    console.log("Richie berotot")
  });
