d0fe130be5770210500e77f5fa8a7c5bff38be39
Richie Christian <2511500001@mahasiswa.atmaluhur.ac.id>
2025-12-24 12:12:44 +0700

    menambahkan paramete cid untuk link delete di read.php

--- pertemuan-13/README.md
+++ pertemuan-13/README.md
@@ -8,6 +8,9 @@ Hari ini, Rabu 24 Desember 2025, materi terakhir untuk PWD saya mempelajari:
 <li>import sql bahan lat pertemuan 13</li>
 <li>menambahkan link delete pada read.php dan buat file baru dengan nama proses_delete.php</li>
 <li>mengcopy kode proses_update.php ke proses_delete.php</li>
+<li>meneysuaikan kode di proses_delete.php untuk delete</li>
+<li>menambahkan paramete cid untuk link delete di read.php</li>
+<li>memperbaiki file proses_delete.php agar menggunakan parameter cid</li>
 <li></li>
-<li></li>
+v
 </ol>
--- pertemuan-13/read.php
+++ pertemuan-13/read.php
@@ -45,8 +45,9 @@
   <?php while ($row = mysqli_fetch_assoc($q)): ?>
     <tr>
       <td><?= $i++ ?></td>
-      <td><a href="edit.php?cid=<?= (int)$row['cid']; ?>">Edit</a></td>
-      <td><a href="proses_delete.php">Delete</a></td>
+      <td><a href="edit.php?cid=<?= (int)$row['cid']; ?>">Edit</a>
+          <a href="proses_delete.php?cid=<?= (int)$row['cid']; ?>">Delete</a>
+</td>
       <td><?= $row['cid']; ?></td>
       <td><?= htmlspecialchars($row['cnama']); ?></td>
       <td><?= htmlspecialchars($row['cemail']); ?></td>
